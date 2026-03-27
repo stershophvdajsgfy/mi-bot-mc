e6a0edde9aa74f87bc4ce23fef0ac7d5264fbfc4
stershophvdajsgfy <stershoptshepo@gmail.com>
2026-03-27 00:47:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const mineflayer = require('mineflayer')
 const http = require('http')
 
+// EL ENGAÑO PARA RENDER (Obligatorio)
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Bot Operativo\n');
+  res.end('Sistema Protegido\n');
 }).listen(10000); 
 
 const botArgs = {
   host: 'PovreZuela.aternos.me', 
-  username: 'ElBotEterno',
+  username: 'Santi_Miner_01', // <--- Pon un nombre de persona real aquí
   version: '1.21.11'
 };
 
@@ -16,19 +17,28 @@ function createBot() {
   const bot = mineflayer.createBot(botArgs);
 
   bot.on('spawn', () => {
-    console.log('¡Bot adentro y estable!');
-    setInterval(() => {
+    console.log('¡Bot Camuflado Adentro!');
+    
+    // FUNCIÓN DE SALTO ALEATORIO (Anti-detección)
+    function randomJump() {
       bot.setControlState('jump', true);
       setTimeout(() => bot.setControlState('jump', false), 500);
-    }, 60000);
+      
+      // Vuelve a saltar en un tiempo aleatorio entre 40 y 80 segundos
+      let nextJump = Math.floor(Math.random() * (80000 - 40000 + 1)) + 40000;
+      setTimeout(randomJump, nextJump);
+    }
+    randomJump();
   });
 
   bot.on('end', () => {
-    console.log('Salió, esperando 2 minutos para no molestar...');
-    setTimeout(createBot, 120000); 
+    // Si lo sacan, esperamos 5 MINUTOS (300000 ms)
+    // Esto es lo que más evita el ban de IP
+    console.log('Esperando 5 minutos para que Aternos se olvide de mí...');
+    setTimeout(createBot, 300000); 
   });
 
-  bot.on('error', (err) => console.log('Error de conexión: ' + err));
+  bot.on('error', (err) => console.log('Error: ' + err));
 }
 
 createBot();
