d4e47fb685bed438888e324b30236a0c99342bd0
stershophvdajsgfy <stershoptshepo@gmail.com>
2026-03-27 00:30:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const mineflayer = require('mineflayer')
 const http = require('http')
 
-// Mantenemos a Render feliz sin que se apague
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Bot Silencioso Operativo\n');
+  res.end('Bot Operativo\n');
 }).listen(10000); 
 
 const botArgs = {
@@ -13,30 +12,23 @@ const botArgs = {
   version: '1.21.11'
 };
 
-let bot;
-
 function createBot() {
-  bot = mineflayer.createBot(botArgs);
+  const bot = mineflayer.createBot(botArgs);
 
   bot.on('spawn', () => {
-    // Ya no dice nada en el chat al entrar, solo sale en la consola de Render
-    console.log('Bot en el server (Silencioso)');
-    
-    // Solo salta cada 1 minuto para que no lo saquen, pero SIN HABLAR
+    console.log('¡Bot adentro y estable!');
     setInterval(() => {
       bot.setControlState('jump', true);
       setTimeout(() => bot.setControlState('jump', false), 500);
     }, 60000);
   });
 
-  // Si lo sacan, esperamos 1 MINUTO entero antes de volver a entrar
-  // Así no spamea el chat de "ElBotEterno se unió al juego" cada rato
   bot.on('end', () => {
-    console.log('Salió del server, esperando 60 segundos...');
-    setTimeout(createBot, 60000); 
+    console.log('Salió, esperando 2 minutos para no molestar...');
+    setTimeout(createBot, 120000); 
   });
 
-  bot.on('error', (err) => console.log('Error: ' + err));
+  bot.on('error', (err) => console.log('Error de conexión: ' + err));
 }
 
 createBot();
